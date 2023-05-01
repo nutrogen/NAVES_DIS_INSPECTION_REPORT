08c2ee9e251d183d162f6ec9cd4d7c29781320c8
nutrogen <81790274+nutrogen@users.noreply.github.com>
2023-05-01 13:01:05 +0900

    Add files via upload

---
+++ DisInspectionReportController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using NavesPortalforWebWithCoreMvc.Controllers.AuthFromIntranetController;
+
+namespace NavesPortalforWebWithCoreMvc.Controllers.DIS
+{
+    [Authorize]
+    [CheckSession]
+    public class DisInspectionReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
